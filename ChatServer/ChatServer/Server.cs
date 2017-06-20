c2bd23bc71a4aedda8d44738c67a7038cc802caa
Boyce <joshuadboyce@gmail.com>
2017-06-20 17:07:08 -0500

    Chat Room Revamp

--- ChatServer/ChatServer/Server.cs
+++ ChatServer/ChatServer/Server.cs
@@ -12,7 +12,6 @@ namespace ChatServer
 {
     public class ChatServer
     {
-
         const int portNumber = 11111;
         public IPAddress localIPParsed = IPAddress.Any;
         public TcpListener tcpListener;
@@ -31,7 +30,7 @@ namespace ChatServer
 
         public void Run()
         {
-            Console.WriteLine("\nThe chat server access key is: " + GetIP() + "\n\nListening for Connection Requests...");
+            Console.WriteLine("\nThe chat server access key is: " + GetIP() + "\n\nListening for Connection Requests...");   /* 1 > */
             try
             {
                 tcpListener.Start();
@@ -41,12 +40,12 @@ namespace ChatServer
                 Console.WriteLine(e);
                 Console.ReadKey();
             }
-            Task.Run(() => ListenForConnections());
-            Task.Run(() => PrintQueue());
+            Task.Run(() => ListenForConnections());   /* 2 > */
+            Task.Run(() => PrintQueue());   /* 7 > */
         }
 
 
-        public string GetIP()
+        public string GetIP()   /* 1 */
         {
             string localIPv4 = null;
             string hostName = Dns.GetHostName();
@@ -59,18 +58,18 @@ namespace ChatServer
         }
 
 
-        public void ListenForConnections()
+        public void ListenForConnections()   /* 2 */
         {
             while (true)
             {
                 TcpClient newClient = tcpListener.AcceptTcpClient();
                 Console.WriteLine("\nNew Connection Made");
-                Task.Run(() => LogUsername(newClient));
+                Task.Run(() => LogUsername(newClient));   /* 3 > */
             }
         }
         
 
-        public void LogUsername(TcpClient client)
+        public void LogUsername(TcpClient client)   /* 3 */
         {
             Console.WriteLine("\nRequesting client username...");
             try
@@ -78,8 +77,8 @@ namespace ChatServer
                 SendMessage(client, "\nWelcome to the chat room! Please enter a username:");
                 string username = ReceiveMessage(client);
                 clientDatabase.Add(username, client);
-                AnnounceNewUser(username);
-                Task.Run(() => ListenForMessages(username, client));
+                AnnounceNewUser(username);   /* 4 > */
+                Task.Run(() => ListenForMessages(username, client));  /* 5 > */
             }
             catch (Exception FailedConnection)
             {
@@ -90,26 +89,26 @@ namespace ChatServer
         }
 
 
-        public void AnnounceNewUser(string username)
+        public void AnnounceNewUser(string username)   /* 4 */
         {
             logger.WriteToFile("User \"" + username + "\" has joined the chat room.");
             Console.WriteLine("\nUsername \"{0}\" has been saved.", username);
             foreach (var client in clientDatabase)
             {
-                SendMessage(client.Value, username + "* has joined the chat room.");
+                SendMessage(client.Value, username + "* has joined the chat room.");   /* ^ */
             }
         }
 
 
-        public void ListenForMessages(string username, TcpClient client)
+        public void ListenForMessages(string username, TcpClient client)   /* 5 */
         {
             while (true)
             {
                 try
                 {
-                    string chatMessage = username + "@: " + ReceiveMessage(client);
+                    string chatMessage = username + "@: " + ReceiveMessage(client);   /* 6 > */
                     Console.WriteLine("\nReceived message from {0}", username);
-                    messageQueue.Enqueue(chatMessage);
+                    messageQueue.Enqueue(chatMessage);   /* ^ */
                 }
                 catch (Exception FailedConnection)
                 {
@@ -126,43 +125,43 @@ namespace ChatServer
         }
 
 
-        public string ReceiveMessage(TcpClient client)
+        public string ReceiveMessage(TcpClient client)   /* 6 */
         {
                 NetworkStream reader = client.GetStream();
                 byte[] buffer = new byte[client.ReceiveBufferSize];
                 int bytesRead = reader.Read(buffer, 0, client.ReceiveBufferSize);
                 string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                return message;
+                return message;   /* ^ */
         }
 
 
-        public void PrintQueue()
+        public void PrintQueue()   /* 7 */
         {
             while (true)
             {
                 if(messageQueue.Count > 0)
                 {
-                    BroadcastMessage(messageQueue.Dequeue());
+                    BroadcastMessage(messageQueue.Dequeue());   /* 8 > */
                 }
             }
         }
        
 
-        public void BroadcastMessage(string message)
+        public void BroadcastMessage(string message)   /* 8 */
         {
             if (clientDatabase.Count > 0)
             {
                 logger.WriteToFile(message);
                 foreach (var client in clientDatabase)
                 {
-                    SendMessage(client.Value, message);
+                    SendMessage(client.Value, message);   /* 9 > */
                     Console.WriteLine("\nMessage sent to: {0}", client.Key);
                 }
             }
         }
-    
 
-        public void SendMessage(TcpClient client, string text)
+
+        public void SendMessage(TcpClient client, string text)  /* 10 */
         {
                 NetworkStream writer = client.GetStream();
                 byte[] message = ASCIIEncoding.ASCII.GetBytes(text);
--- ChatServer/ChatServer/TextLogger.cs
+++ ChatServer/ChatServer/TextLogger.cs
@@ -12,3 +12,244 @@ namespace ChatServer
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const string localIP = "10.2.20.50";
+
+
+/* namespace Server
+{
+    class Program
+    {
+        const int portNumber = 54321;
+        const string serverIP = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+
+
+
+            //---listen at the specified IP and port no.---
+
+            IPAddress localIPAddress = IPAddress.Parse(serverIP);
+            TcpListener listener = new TcpListener(localIPAddress, portNumber);
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+
+
+
+
+            //---incoming client connected---
+
+            TcpClient client = listener.AcceptTcpClient();
+
+
+
+
+
+            //---get the incoming data through a network stream---
+
+            NetworkStream networkStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+
+
+
+
+            //---read incoming stream---
+
+            int bytesRead = networkStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+
+
+
+
+            //---convert the data received into a string---
+
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+
+
+
+
+
+            //---write back the text to the client---
+
+            Console.WriteLine("Sending back : " + dataReceived);
+            networkStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+
+
+        }
+    }
+}
+*/
+
+
+
+
+
+//namespace ChatServer
+//{
+//class Program
+////{
+//static byte[] Buffer { get; set; }
+//static Socket socket;
+//static void Main(string[] args)
+//{
+//    socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+//    socket.Bind(new IPEndPoint(IPAddress.Any, 1234));
+//    socket.Listen(100);
+
+//    Socket accepted = socket.Accept();
+//    Buffer = new byte[accepted.SendBufferSize];
+//    int bytesRead = accepted.Receive(Buffer);
+//    byte[] formatted = new byte[bytesRead];
+//    for (int i = 0; i < bytesRead; i++)
+//    {
+//        formatted[i] = Buffer[i];
+//    }
+//    string stringData = Encoding.ASCII.GetString(formatted);
+//    Console.WriteLine(stringData + "\r\n");
+//    Console.ReadLine();
+
+//    socket.Close();
+//    accepted.Close();
+
+//}
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    TcpListener serverSocket = new TcpListener(8888);
+//    int requestCount = 0;
+//    TcpClient clientSocket = default(TcpClient);
+//    serverSocket.Start();
+//    Console.WriteLine(" >> Server Started");
+//    clientSocket = serverSocket.AcceptTcpClient();
+//    Console.WriteLine(" >> Accept connection from client");
+//    requestCount = 0;
+
+//    while ((true))
+//    {
+//        try
+//        {
+//            requestCount = requestCount + 1;
+//            NetworkStream networkStream = clientSocket.GetStream();
+//            byte[] bytesFrom = new byte[10025];
+//            networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+//            string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+//            dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+//            Console.WriteLine(" >> Data from client - " + dataFromClient);
+//            string serverResponse = "Last Message from client" + dataFromClient;
+//            Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+//            networkStream.Write(sendBytes, 0, sendBytes.Length);
+//            networkStream.Flush();
+//            Console.WriteLine(" >> " + serverResponse);
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine(ex.ToString());
+//        }
+//    }
+
+//    clientSocket.Close();
+//    serverSocket.Stop();
+//    Console.WriteLine(" >> exit");
+//    Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
